4d70917c6a1ee42b17758a695fa60c63f9a2414f
mrrickrey <mrrickrey@gmail.com>
2017-03-24 18:09:04 -0400

    sing in ui custom classes complete

---
+++ socialapp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  socialapp
+//
+//  Created by Enrique Reyes on 3/23/17.
+//  Copyright © 2017 Enrique Reyes. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
