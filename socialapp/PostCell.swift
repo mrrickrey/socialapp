3328898980f07dc9b887040ea99bcce73630c84a
mrrickrey <mrrickrey@gmail.com>
2017-11-15 12:54:11 -0500

    update

---
+++ socialapp/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  socialapp
+//
+//  Created by Enrique Reyes on 4/11/17.
+//  Copyright © 2017 Enrique Reyes. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
